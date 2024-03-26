5f41bebd90a616ad54a076ff8bcd819b58347d46
Elias Carlson <carlson.elias@gmail.com>
2024-03-26 14:48:39 +0100

    Added ability to have separate grids with gaps between

--- SFC/grid_attention.py
+++ SFC/grid_attention.py
@@ -6,7 +6,7 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
+import math
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="")
@@ -120,39 +120,42 @@ def draw_grid(frame, cell_positions, line_color=(255, 255, 255), line_thickness=
         )
 
 
-def calculate_cell_positions(image, config):
+def calculate_grid_cell_positions(image, grid_configs):
     h, w, _ = image.shape
+    all_cell_positions = []
 
-    # Convert proportional positions to pixel positions
-    start_x = int(w * config["grid_top_left"][0])
-    start_y = int(h * config["grid_top_left"][1])
-    end_x = int(w * config["grid_bottom_right"][0])
-    end_y = int(h * config["grid_bottom_right"][1])
+    for grid_config in grid_configs:
+        # Convert proportional positions to pixel positions for each grid
+        start_x = int(w * grid_config["top_left"][0])
+        start_y = int(h * grid_config["top_left"][1])
+        end_x = int(w * grid_config["bottom_right"][0])
+        end_y = int(h * grid_config["bottom_right"][1])
 
-    cell_width = (end_x - start_x) // config["grid_num_cols"]
-    cell_height = (end_y - start_y) // config["grid_num_rows"]
+        cell_width = (end_x - start_x) // grid_config["cols"]
+        cell_height = (end_y - start_y) // grid_config["rows"]
 
-    cell_positions = []
+        cell_positions = []
 
-    if config["grid_direction"] == "right":
-        col_range = range(config["grid_num_cols"])
-    else:
-        col_range = range(config["grid_num_cols"] - 1, -1, -1)
+        if grid_config["direction"] == "right":
+            col_range = range(grid_config["cols"])
+        else:
+            col_range = range(grid_config["cols"] - 1, -1, -1)
 
-    for row in range(config["grid_num_rows"]):
-        for col in col_range:
-            cell_start_x = start_x + col * cell_width
-            cell_start_y = start_y + row * cell_height
+        for row in range(grid_config["rows"]):
+            for col in col_range:
+                cell_start_x = start_x + col * cell_width
+                cell_start_y = start_y + row * cell_height
 
-            cell_end_x = cell_start_x + cell_width
-            cell_end_y = cell_start_y + cell_height
+                cell_end_x = cell_start_x + cell_width
+                cell_end_y = cell_start_y + cell_height
 
-            cell_positions.append(
-                ((cell_start_x, cell_start_y), (cell_end_x, cell_end_y))
-            )
+                cell_positions.append(
+                    ((cell_start_x, cell_start_y), (cell_end_x, cell_end_y))
+                )
 
-    return cell_positions
+        all_cell_positions.extend(cell_positions)
 
+    return all_cell_positions
 
 def calculate_cell_values(frame, cell_positions, saliency_threshold):
     h, w = frame.shape[:2]
@@ -207,16 +210,24 @@ def annotate_frame(
     cv2.putText(frame, text, position, font, font_scale, font_color, thickness)
 
 
-def save_csv(mean_attention_map, output_path, config):
+def save_csv(mean_attention_map, output_path, grids_config):
     csv_path = os.path.join(output_path, "cell_values.csv")
     with open(csv_path, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=";")
-        number_of_cells = config["grid_num_rows"] * config["grid_num_cols"]
-        writer.writerow(["frame_id"] + [f"cell{i+1}" for i in range(number_of_cells)])
+
+        # Calculate the total number of cells based on the individual grids
+        cell_headers = []
+        cell_counter = 0
+        for grid in grids_config['grids']:
+            cells_in_grid = grid["rows"] * grid["cols"]
+            cell_headers.extend([f"cell{cell_counter + i + 1}" for i in range(cells_in_grid)])
+            cell_counter += cells_in_grid
+
+        writer.writerow(["frame_id"] + cell_headers)
+
         for key, values in mean_attention_map.items():
             row = [key] + values
             writer.writerow(row)
-
 
 def save_config(output_path, data_config, event_config):
     config_path = os.path.join(output_path, "config.txt")
@@ -228,15 +239,30 @@ def save_config(output_path, data_config, event_config):
             line = f"{key}: {value}\n"
             f.write(line)
 
+def get_total_cells(event_config) -> int:
+    total_cells = 0
+    for grid in event_config['grids']:
+        cells = grid["rows"] * grid["cols"]
+        total_cells += cells
+    return total_cells
 
-def save_cell_value_subplots(mean_attention_map, output_path, display_results, event_config):
+
+def save_cell_value_subplots(mean_attention_map, output_path, display_results, total_cells):
+    # Find a reasonable number of rows and columns for the subplot grid
+    plot_cols = math.ceil(math.sqrt(total_cells))
+    plot_rows = math.ceil(total_cells / plot_cols)
+
     fig, axs = plt.subplots(
-        nrows=event_config["grid_num_rows"],
-        ncols=event_config["grid_num_cols"],
-        figsize=(event_config["grid_num_cols"] * 4, event_config["grid_num_rows"] * 3),
+        nrows=plot_rows,
+        ncols=plot_cols,
+        figsize=(plot_cols * 4, plot_rows * 3),
     )
 
-    for cell_index, ax in enumerate(axs.flat):
+    # Flatten the axes array for easy iteration, in case of a single row/column
+    axs = np.atleast_2d(axs).reshape(-1)
+
+    for cell_index in range(total_cells):
+        ax = axs[cell_index]
         ax.plot(
             [value[cell_index] for value in mean_attention_map.values()],
             label=f"Cell {cell_index+1}",
@@ -245,6 +271,10 @@ def save_cell_value_subplots(mean_attention_map, output_path, display_results, e
 
         if cell_index == 0:
             ax.legend()
+
+    # Hide unused subplots if any
+    for ax in axs[total_cells:]:
+        ax.axis('off')
 
     fig.text(0.5, 0.04, "Frame", ha="center")
     fig.text(0.04, 0.5, "Mean Attention", va="center", rotation="vertical")
@@ -259,24 +289,23 @@ def save_cell_value_subplots(mean_attention_map, output_path, display_results, e
         plt.show()
     else:
         plt.close()
-
-def save_combined_plot(mean_attention_map, output_path, display_results, event_config):
-    fig, ax = plt.subplots(figsize=(10, 7))  # Adjust the figure size as needed
+def save_combined_plot(mean_attention_map, output_path, display_results, total_cells):
+    fig, ax = plt.subplots(figsize=(10, 7))
 
     # Generate a color cycle or define a list of colors if specific ones are desired
     color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
-    for cell_index in range(event_config["grid_num_rows"] * event_config["grid_num_cols"]):
+    for cell_index in range(total_cells):
         ax.plot(
             [value[cell_index] for value in mean_attention_map.values()],
             label=f"Cell {cell_index + 1}",
-            color=color_cycle[cell_index % len(color_cycle)]  # Cycle through colors
+            color=color_cycle[cell_index % len(color_cycle)]
         )
 
     ax.set_title("Cell Values Over Time")
     ax.set_xlabel("Frame")
     ax.set_ylabel("Mean Attention")
-    ax.legend()  # Show legend for all lines
+    ax.legend()
 
     plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9)
 
@@ -326,7 +355,7 @@ def process_video_and_generate_attention_map(
     frame_number = 0
     mean_attention_map = {}
 
-    cell_positions = calculate_cell_positions(frame_heatmap, event_config)
+    cell_positions = calculate_grid_cell_positions(frame_heatmap, event_config["grids"])
 
     while ret_heatmap and ret_original:
         frame_number += 1
@@ -363,7 +392,6 @@ def process_video_and_generate_attention_map(
 
     return mean_attention_map
 
-
 def main(data_path, output_path, data_config, event_config, display_results):
     assert os.path.exists(data_path), f"Dataset path {data_path} does not exist."
     assert os.path.exists(output_path), f"Output path {output_path} does not exist."
@@ -373,6 +401,8 @@ def main(data_path, output_path, data_config, event_config, display_results):
         for name in os.listdir(data_path)
         if os.path.isdir(os.path.join(data_path, name))
     ]
+
+    total_cells = get_total_cells(event_config)
 
     pbar = tqdm(video_dirs, desc="Processing videos")
     for video_id in pbar:
@@ -398,9 +428,9 @@ def main(data_path, output_path, data_config, event_config, display_results):
 
                 save_csv(mean_attention_map, target_path, event_config)
                 save_cell_value_subplots(
-                    mean_attention_map, target_path, display_results, event_config
+                    mean_attention_map, target_path, display_results, total_cells
                 )
-                save_combined_plot(mean_attention_map, target_path, display_results, event_config)
+                save_combined_plot(mean_attention_map, target_path, display_results, total_cells)
             else:
                 print("Skipped. Source or heatmap video does not exist.")
         else:
